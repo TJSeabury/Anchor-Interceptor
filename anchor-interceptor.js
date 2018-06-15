2754f491d2ec15a90d908b941c59ef0eda599bce
Tyler <tylerseabury@gmail.com>
2018-06-15 09:33:20 -0400

    Updated to fix two bugs.
    Added: ability to target only specifc classed anchors.
    Fixed: navigation occurs if there is a hashtag in the address bar and
    the anchor links to the same page.

--- anchor-interceptor.js
+++ anchor-interceptor.js
@@ -1,159 +1,189 @@
-/*
-* Anchor Interceptor
-*/
-(
-    function( $ )
-    {
-		'use strict';
-        window.addEventListener(
-            'load',
-            () =>
-            {
-				// Interceptor constructor must be called with a css selector string 
-				// that targets the header and/or nav to be used when calculating 
-				// scroll offsets; e.g., '.mk-header.header-style-1'.
-                let interceptor = new AnchorInterceptor( '.mk-header' );
-				if ( true === interceptor.checkForTarget() )
+'use strict';
+window.addEventListener(
+	'DOMContentLoaded',
+	() =>
+	{
+		/**
+		 * Interceptor constructor must be called with a css selector string 
+		 * that targets the header or nav that is used when calculating 
+		 * scroll offsets; e.g., '.mk-header.header-style-1'; an array of
+		 * classes must also be provided for interception targets. If an empty
+		 * targets array is provided all click events on anchors will be intercepted.
+		 * ( Quite obviously, that can lead to undesireble conflicting behavior, but one 
+		 * may do that if so desired. )
+		 */
+		let interceptor = new AnchorInterceptor( '.mk-header .mk-header-holder', ['smoothScrollThis','js-smooth-scroll'] );
+		if ( true === interceptor.checkForTarget() )
+		{
+			window.addEventListener(
+				'load',
+				() =>
 				{
 					interceptor.doAnimate( interceptor.getTarget() );
 				}
-				window.addEventListener(
-					'click',
-					function( ev )
-					{
-						interceptor.clickHandler( ev );
-					}
-				);
-            }
-        );
-
-        class AnchorInterceptor
-        {
-            constructor( headerQueryString )
-            {
-				this.header = (
-					function( hqs )
-					{
-						let header = document.querySelector( hqs );
-						if ( undefined !== header || null !== header )
-						{
-							return header;
-						}
-					}
-				)( headerQueryString );
-				this.headerOffset = this.header.offsetHeight;
-            }
-			
-            setTarget( anchorTarget )
-            {
-                sessionStorage.setItem( 'anchorTarget', anchorTarget );
-            }
-
-            getTarget()
-            {
-                return sessionStorage.getItem( 'anchorTarget' );
-            }
-			removeTarget()
+			);
+		}
+		window.addEventListener(
+			'click',
+			function( ev )
 			{
-				if ( sessionStorage.getItem( 'anchorTarget' ) )
+				interceptor.clickHandler( ev );
+			}
+		);
+	}
+);
+
+/**
+ * Anchor Interceptor
+ * © Copyright 2017-2018, Tyler Seabury, All Rights reserved.
+ * @author Tyler Seabury, tylerseabury@gmail.com
+ * @authorURL https://github.com/TJSeabury/
+ * @version 1.1.0
+ */
+class AnchorInterceptor
+{
+	constructor( headerQueryString, targets = [] )
+	{
+		this.header = (
+			function( hqs )
+			{
+				let header = document.querySelector( hqs );
+				if ( undefined !== header || null !== header )
 				{
-					sessionStorage.removeItem( 'anchorTarget' );
+					return header;
 				}
 			}
+		)( headerQueryString );
+		this.headerOffset = this.header.offsetHeight;
+		this.targets = targets;
+	}
 
-            checkForTarget()
-            {
-            	if ( 
-					null !== sessionStorage.getItem( 'anchorTarget' ) &&
-					'' !== sessionStorage.getItem( 'anchorTarget' )
-				)
+	setTarget( anchorTarget )
+	{
+		sessionStorage.setItem( 'anchorTarget', anchorTarget );
+	}
+
+	getTarget()
+	{
+		return sessionStorage.getItem( 'anchorTarget' );
+	}
+	removeTarget()
+	{
+		if ( sessionStorage.getItem( 'anchorTarget' ) )
+		{
+			sessionStorage.removeItem( 'anchorTarget' );
+		}
+	}
+
+	checkForTarget()
+	{
+		if ( 
+			null !== sessionStorage.getItem( 'anchorTarget' ) &&
+			'' !== sessionStorage.getItem( 'anchorTarget' )
+		)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	getLinkFromTarget( target ) {
+		while ( target.localName !== 'a' ) {
+			if ( ! target.localName )
+			{
+				return false;
+			}
+			target = target.parentNode;
+		} 
+		return target;
+	}
+
+	getStripURL( url )
+	{
+		return url.substring( 0, url.indexOf( '#' ) );
+	}
+
+	getTargetID( link )
+	{
+		return link.href.substring( link.href.indexOf( '#' ) + 1 );
+	}
+
+	doNavigate( url )
+	{
+		window.location.href = url;
+	}
+
+	doAnimate( id )
+	{
+		const anchorTarget = document.getElementById( id );
+		if ( undefined === anchorTarget )
+		{
+			return;
+		}
+		const deltaY = this.getScrollAmount( anchorTarget ) - this.headerOffset;
+		if ( 0 === deltaY )
+		{
+			this.removeTarget();
+			return;
+		}
+		jQuery("html, body").animate(
+			{ scrollTop: deltaY + 'px' },
+			999
+		);
+		this.removeTarget();
+		return false;
+	}
+
+	getScrollAmount( t )
+	{
+		let windowOffset = t.getBoundingClientRect().top;
+		let elementOffset = window.scrollY + windowOffset;
+		return elementOffset;
+	}
+	
+	hasIntersectingClass( A, B )
+	{
+		for ( let a of A )
+		{
+			for ( let b of B )
+			{
+				if ( a === b )
 				{
 					return true;
 				}
-				return false;
-            }
-			
-			getLinkFromTarget( target ) {
-				while ( target.localName !== 'a' ) {
-					if ( ! target.localName )
-					{
-						return false;
-					}
-					target = target.parentNode;
-				} 
-				return target;
 			}
-			
-			getStripURL( link )
-			{
-				let url = link.href;
-				url = url.substring( 0, url.indexOf( '#' ) );
-				return url;
-			}
-			
-			getTargetID( link )
-			{
-				return link.href.substring( link.href.indexOf( '#' ) + 1 );
-			}
-            
-            doNavigate( url )
-            {
-				window.location.href = url;
-            }
-			
-			doAnimate( id )
-			{
-				const anchorTarget = document.getElementById( id );
-				if ( undefined === anchorTarget )
-				{
-					return;
-				}
-				const deltaY = this.getScrollAmount( anchorTarget ) - this.headerOffset;
-				if ( 0 === deltaY )
-				{
-					this.removeTarget();
-					return;
-				}
-				$("html, body").animate(
-					{ scrollTop: deltaY + 'px' },
-					500
-				);
-				this.removeTarget();
-			}
-			
-			getScrollAmount( t )
-			{
-				let windowOffset = t.getBoundingClientRect().top;
-				let elementOffset = window.scrollY + windowOffset;
-				return elementOffset;
-			}
-            
-            clickHandler( event )
-            {
-				const link = this.getLinkFromTarget( event.target );
-				if ( ! link )
-				{
-					return;
-				}
-				event.preventDefault();
-				const url = link.href;
-				const cleanURL = this.getStripURL( link );
-				const id = this.getTargetID( link );
-				if ( -1 === url.indexOf('#') )
-				{
-					this.doNavigate( url );
-				}
-				else if ( window.location.href === cleanURL )
-				{
-					this.doAnimate( id );
-				}
-				else
-				{
-					this.setTarget( id );
-					this.doNavigate( cleanURL );
-				}
-            }
-        }
+		}
+		return false;
+	}
 
-    }
-)( jQuery );
+	clickHandler( event )
+	{
+		const link = this.getLinkFromTarget( event.target );
+		if ( ! link )
+		{
+			return;
+		}
+		if ( this.targets.length > 0 && ! this.hasIntersectingClass( Array.from( link.classList ), this.targets ) )
+		{
+			return;
+		}
+		event.preventDefault();
+		const url = link.href;
+		const cleanURL = this.getStripURL( link.href );
+		const cleanLocatation = this.getStripURL( window.location.href );
+		const id = this.getTargetID( link );
+		if ( -1 === url.indexOf('#') )
+		{
+			this.doNavigate( url );
+		}
+		else if ( cleanURL === cleanLocatation )
+		{
+			this.doAnimate( id );	
+		}
+		else
+		{
+			this.setTarget( id );
+			this.doNavigate( cleanURL );
+		}
+	}
+}
