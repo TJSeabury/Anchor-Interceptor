db42f1c0dce0242340825c81de5604757cb53cb1
TJSeabury <tylerseabury@gmail.com>
2022-02-22 13:15:35 -0500

    moved instructions to readme

--- src/AnchorInterceptor.ts
+++ src/AnchorInterceptor.ts
@@ -23,25 +23,6 @@ export class AnchorInterceptor {
     a: Animator;
     header: HTMLElement;
 
-    /**
-     * Interceptor constructor may be called with a css selector string 
-     * that targets the header or nav that is used when calculating 
-     * scroll offsets; e.g., '.mk-header.header-style-1'; an array of
-     * classes must also be provided for interception targets. If an empty
-     * targets array is provided all click events on anchors will be intercepted.
-     * ( Quite obviously, that can lead to undesireble conflicting behavior, but one 
-     * may do that if they wished. )
-     * Usage example: 
-     * 
-     * const interceptor = new AnchorInterceptor(
-     *      false,
-     *      [
-     *          'smoothScrollThis',
-     *          'js-smooth-scroll'
-     *      ]
-     * );
-     *
-     */
     constructor(headerQueryString: string, targets = [AnchorInterceptor.targetClassname]) {
         this.a = window.a || (window.a = new Animator(20));
 
